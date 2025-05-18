f1ffb4e9a5433bb689256c49b5132dd4e43569c5
ngoct <ngoctranged99@gmail.com>
2025-05-18 17:57:02 +0700

    lưu tạm trước khi rebase

--- server/memoria/settings.py
+++ server/memoria/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-GEMINI_API_KEY = "your_gemini_api_key_here"  # Replace with your actual Gemini API key
+GEMINI_API_KEY = "your_gemini_api_key_here"  # Replace with your actual Gemini API keygit
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
